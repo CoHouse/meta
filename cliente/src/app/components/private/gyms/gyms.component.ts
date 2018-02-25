4275d7b561bf51008062e4e617f89c5ec66ebed3
CoHouse <webmastersirius38@gmail.com>
2018-02-25 00:15:51 -0600

    Definiciones Luis 25-02-18

---
+++ cliente/src/app/components/private/gyms/gyms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gyms',
+  templateUrl: './gyms.component.html',
+  styles: []
+})
+export class GymsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
