84f0fa71bbd5ffa92512312b619737f1023f1714
Cohouse <webmastersirius38@gmail.com>
2018-03-26 18:00:00 -0600

    Cambios del 26-03-18

--- cliente/src/app/components/public/videos/videos.component.ts
+++ cliente/src/app/components/public/videos/videos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from "@angular/http";
+import { VideoService } from '../../../services/video.service';
+import { Changer } from '../../../interfaces/changer.interface';
+import { Router, ActivatedRoute } from '@angular/router';
+declare var $:any;
 
 @Component({
   selector: 'app-videos',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideosComponent implements OnInit {
 
-  constructor() { }
+  public categories;
+  public videos;
+  public category;
 
-  ngOnInit() {
+  constructor( public _video:VideoService, public _activatedRoute:ActivatedRoute ){
+    this._activatedRoute.params.subscribe( params => {
+      this.category = params['category'];
+    });
+
+
+    this._video.getCategories().subscribe( result => {
+      this.categories = result['showBlogCategories'];
+    }, error => {
+      var errorMessage = <any>error;
+    });
+
+    // this._blog.getPosts().subscribe( result => {
+    //   this.posts = result['showPosts'];
+    // }, error => {
+    //   var errorMessage = <any>error;
+    // });
+
+
   }
+
+  ngOnInit() { }
 
 }
