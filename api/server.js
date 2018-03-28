b993df0a44b4816149b89f6aed8dd931871af764
Cohouse <webmastersirius38@gmail.com>
2018-03-28 17:57:11 -0600

    cambios del 28-03-18, todo en línea

---
+++ api/server.js
@@ -0,0 +1,30 @@
+var app = require('./app'); // this is your express app
+var http = require('http'); // 3. HTTP server
+var mongoose = require("mongoose");
+
+/**
+ * Get port from environment and store in Express.
+ */
+var port = process.env.PORT; // 2. Using process.env.PORT
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app);
+
+/* Conexión a base de datos */
+mongoose.connect("mongodb://mbchanger:5al1x9SP45EDus3Ac5@ds259768.mlab.com:59768/metadb", (error, response) => {
+  if( error ){
+    throw error;
+  } else {
+    server.listen( port, function(){
+      console.log("Server Online");
+    })
+  }
+} );
+
+/**
+* Listen on provided port, on all network interfaces.
+*/
+// server.listen(port);
--- cliente/src/app/global_route.ts
+++ cliente/src/app/global_route.ts
@@ -1,3 +1,4 @@
 export var Ruta = {
-  url: "http://localhost:2222/api/"
+  // url: "http://localhost:2222/api/" /* Development */
+  url: "http://mbchanger.fr.openode.io/api/" /* Production */
 }
